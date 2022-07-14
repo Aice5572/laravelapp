c61ebfcefc4035fb97798667842d05c9d6f20382
hide- <hide.0729.mori@gmail.com>
2022-07-14 09:48:43 +0900

    4-1 ビューとコントローラーの修正まで

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Middleware\FightMiddleware;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -45,6 +46,7 @@ E0F;
 Route::get('hello', 'App\Http\Controllers\helloController@index' );
 Route::get('hello/other','App\Http\Controllers\helloController@other' );
 
-//Route::get('Fight/{id?}', 'App\Http\Controllers\FightController@index');
-//Route::post('Fight','App\Http\Controllers\FightController@post');
-Route::get('Fight','App\Http\Controllers\FightController@index');
+
+Route::get('Fight','App\Http\Controllers\FightController@index')
+    -> middleware(FightMiddleware::class);
+
